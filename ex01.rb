c6c08733db81662a57373d85f09d90e70f3e7374
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-18 21:59:51 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -2,8 +2,9 @@
 # My first Ruby program
 
 # function with no paranthesis needed
-def sayHello(name)
-    puts 'Hello ' + name + "!"
+def sayHello(name, son)
+    puts 'Hello ' + son + "!"
+    puts
 end
 
 # calling a function without paran
