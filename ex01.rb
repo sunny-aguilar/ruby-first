2920e095db5e8d01cb4edbe63a3dca0e57345b46
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-18 22:00:35 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -4,7 +4,7 @@
 # function with no paranthesis needed
 def sayHello(name, son)
     puts 'Hello ' + son + "!"
-    puts 'Papa loves you so'
+    puts 'Papa loves you so much!'
 end
 
 # calling a function without paran
