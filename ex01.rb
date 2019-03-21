f0f0909cab1e9516f6dde7dbc75da0d58ca713a5
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-21 11:29:57 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -8,14 +8,14 @@ puts "\n"
 arr = [1,2,3,4,5]
 puts arr.count {|i| i > 2}
 
-# def showArr
-#     myArr = [1,2,3,3,4,5]
-#     puts "# of 3s: #{myArr.count {|i|} i > 3 }"
+def showArr
+    myArr = [1,2,3,3,4,5]
+    puts myArr[0]
 #     index = 0
 #     while index < myArr.length
 #         puts myArr[index]
 #         index += 1
 #     end
-# end
+end
 
 # showArr
