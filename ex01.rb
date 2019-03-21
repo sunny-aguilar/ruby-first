8fa53e71639982c2fe8364d52fed0b34256603da
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-20 21:13:38 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -3,18 +3,8 @@
 
 print "How old are you? "
 age = gets.chomp
-print "How tall are you? "
-height = gets.chomp
-print "How much do you weigh? "
-weight = gets.chomp
+
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
 
-def oldFunction(name)
-    puts "say something"
-    puts "I'm adding 5 + 2 which is equal to #{5 + 2}"
-    puts "Let's pass in some variables"
-    puts "Say #{name}"
-end
-
-oldFunction "sandro"
+ 
