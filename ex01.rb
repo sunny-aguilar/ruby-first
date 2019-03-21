3fbacd28587d1c622bce6bdc8d53b66a0e9a58f0
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-21 11:32:08 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -11,11 +11,11 @@ puts arr.count {|i| i > 2}
 def showArr
     myArr = [1,2,3,3,4,5]
     puts myArr[0]
-#     index = 0
+    index = 0
 #     while index < myArr.length
 #         puts myArr[index]
 #         index += 1
 #     end
 end
 
-# showArr
+showArr
