3c5aecad480af2283bbbd9d0346d77418204b1cf
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-21 08:10:34 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -5,7 +5,8 @@ letters = ["a", "b", "c", 3]
 print letters
 
 def showArr
-    myArr = [1,2,3,4,5]
+    myArr = [1,2,3,3,4,5]
+    myArr.count
     index = 0
     while index < myArr.length
         puts myArr[index]
