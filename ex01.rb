281facff1bc96e6a8a227ed926e8383719ebb60c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-20 21:13:51 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -1,10 +1,8 @@
 # Author: Sandro Aguilar
 # My first Ruby program
 
+
 print "How old are you? "
 age = gets.chomp
 
-
-puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
-
- 
+puts ""
