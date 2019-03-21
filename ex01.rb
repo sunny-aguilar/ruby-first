c7e7be27b9bd18e8f61c0f4619e3357069259a15
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-21 08:16:18 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -5,16 +5,17 @@ letters = ["a", "b", "c", 3]
 print letters
 puts "\n"
 
-arr = []
+arr = [1,2,3,4,5]
+arr.count {|i| i > 3}
 
-def showArr
-    myArr = [1,2,3,3,4,5]
-    puts "# of 3s: #{myArr.count {|i|} i > 3 }"
-    index = 0
-    while index < myArr.length
-        puts myArr[index]
-        index += 1
-    end
-end
+# def showArr
+#     myArr = [1,2,3,3,4,5]
+#     puts "# of 3s: #{myArr.count {|i|} i > 3 }"
+#     index = 0
+#     while index < myArr.length
+#         puts myArr[index]
+#         index += 1
+#     end
+# end
 
 showArr
