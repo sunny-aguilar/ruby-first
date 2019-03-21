da831f8e9a1039a1fb676f5c9ff1a7ace096b01c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-21 12:10:50 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -1,22 +1,4 @@
 # Author: Sandro Aguilar
 # My first Ruby program
 
-letters = ["a", "b", "c", 3]
-print letters
-puts "\n"
-
-arr = [1,2,3,4,5]
-puts arr.count {|i| i > 2}
-
-def showArr
-    myArr = [1,2,3,3,4,5]
-    puts myArr[0]
-    index = 0
-    while index < myArr.length
-        puts myArr[index]
-        index += 1
-    end
-end
-
-showArr
-
+names = []
