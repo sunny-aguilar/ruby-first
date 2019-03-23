8ec8ead83e9c199a02ee043e85ef658684940c81
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-22 20:47:51 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -3,9 +3,15 @@
 
 def fizz_buzz(max)
 
-    
+
 end
 
 print fizz_buzz(20) # => [4, 6, 8, 16, 18]
 puts
 print fizz_buzz(15) # => [4, 6, 8]
+
+# Write a method fizz_buzz(max) that 
+# takes in a number max and returns an 
+# array containing all numbers greater than 
+# 0 and less than max that are divisible by 
+# either 4 or 6, but not both.
