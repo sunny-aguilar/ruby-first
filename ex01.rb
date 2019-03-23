ba1585fa3d890c9129e73e5b3af0d2008468070e
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-22 22:12:50 -0700

    final

--- ex01.rb
+++ ex01.rb
@@ -2,9 +2,3 @@
 # A Ruby program
 
 
-
-# Write a method fizz_buzz(max) that
-# takes in a number max and returns an
-# array containing all numbers greater than
-# 0 and less than max that are divisible by
-# either 4 or 6, but not both.
