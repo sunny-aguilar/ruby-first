8cff054c69504cdac89e10caf3fbf7f07263f22e
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-23 16:15:16 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -2,6 +2,9 @@
 # A Ruby program
 
 print "Enter a number\n>> "
-number = gets.chomp
+number = gets.chomp.to_i
 
 print "Your number is #{number}\n"
+
+maths = 10 * number
+print "Your multiplicationis #"
