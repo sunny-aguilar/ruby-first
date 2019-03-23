15e835e16b4a4b53327a1dc4e9bb06af1df4a113
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-22 20:44:59 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -1,18 +1,4 @@
 # Author: Sandro Aguilar
 # A Ruby program
 
-def sum_elements(arr1, arr2)
-    sum = []
-    i = 0
-
-    while i < arr1.length
-        sum << arr1[i] + arr2[i]
-        i += 1
-    end
-
-    return sum
-end
-
-print sum_elements([7, 4, 4], [3, 2, 11])                            # => [10, 6, 15]
-puts
-print sum_elements(["cat", "pizza", "boot"], ["dog", "pie", "camp"]) # => ["catdog", "pizzapie", "bootcamp"]
+otcamp"]
