c4658f8e91f00bfcf4da7d9b43121c1b03f02b10
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-22 22:12:42 -0700

    final

--- ex01.rb
+++ ex01.rb
@@ -1,23 +1,7 @@
 # Author: Sandro Aguilar
 # A Ruby program
 
-def fizz_buzz(max)
-    divides = []
-    i = 1
 
-    while i < max
-        if (i % 4 == 0 || i % 6 == 0) && !(i % 4 == 0 && i % 6 == 0)
-            divides << i
-        end
-        i += 1
-    end
-
-    return divides
-end
-
-print fizz_buzz(20) # => [4, 6, 8, 16, 18]
-puts
-print fizz_buzz(15) # => [4, 6, 8]
 
 # Write a method fizz_buzz(max) that
 # takes in a number max and returns an
