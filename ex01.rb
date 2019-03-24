edc9a2565eb84066733b5b637bc04ce1364d95e5
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-23 20:47:13 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -3,6 +3,7 @@
 # .index("SA") => returns index if it exists
 # .include?("CA") => returns T/F if its in the array
 # .reverse => reverses an array
+# [n...N] => returns a range in an array
 
 arr = [1,2,3,4]
 print arr
@@ -20,4 +21,4 @@ end
 puts is_palindrome("car")
 
 numArr = ["a", "b", "c", "d", "e"]
-print numArr[0...3]
+print numArr[1...4]
