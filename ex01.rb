d5062c80abbd8cad9e8ce7c318058d67d79f6af9
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-24 10:56:34 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -6,6 +6,8 @@
 # [n..N] => returns a range in an array including N/ ... exludes N
 # .each
 
-(1..10).each do |day|
-    puts day
-end
+# (1..10).each do |day|
+#     puts day
+# end
+
+(1...10).each do { |day| puts day }
