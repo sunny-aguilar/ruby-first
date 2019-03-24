063deb3f82b20423607a1443254c6e24306339b4
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-24 10:43:39 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -6,8 +6,7 @@
 # [n..N] => returns a range in an array including N/ ... exludes N
 # .each
 
-str = ""
-arr.each { |month| puts month}   # || is a block parameter
+str = "Hello World"
 arr.each do |month|
     puts "-----"
     puts month
