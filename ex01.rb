b59e07d25eaa1d7d638b063a81fe1684b68a8dc8
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-24 10:37:06 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -4,7 +4,6 @@
 # .include?("CA") => returns T/F if its in the array
 # .reverse => reverses an array
 # [n..N] => returns a range in an array including N/ ... exludes N
+# .each
 
-l = ->a, b = 28, c = 1{puts (("Love" * a).center(b)) * c}
-l.call(2, 14, 2)
-l.call(3, 14, 2);[7,7,7,6,5,4,3,2,1].map{|x|l.call(x)}
+arr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"]
