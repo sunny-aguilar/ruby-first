8c7d0b7f7fa43358a474a42688548962f63004c2
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-23 19:44:45 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -1,8 +1,4 @@
 # Author: Sandro Aguilar
 # A Ruby program
 
-first, second, third = ARGV
-
-puts "Your first variable is: #{first}"
-puts "Your second variable is: #{second}"
-puts "Your third variable is: #{third}"
+arr = ["SA", "WA", "CA"]
