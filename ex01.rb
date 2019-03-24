72c8d89a6f59e105612d12cd0dc519ecc2472fc0
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-24 16:33:03 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -6,10 +6,6 @@
 # [n..N] => returns a range in an array including N/ ... exludes N
 # .each { |var| puts var }
 # .each do |var| *statements* end
-# .times
+# .times {}
 
-# (1..10).each do |day|
-#     puts day
-# end
-
-5.times { puts "hello" }
+arr = ["a", "b", "c", "d", "e"]
