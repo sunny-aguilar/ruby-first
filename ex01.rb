cbb81966b4279a716ddcae89c34e33f426cd5975
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-23 19:55:31 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -1,5 +1,7 @@
 # Author: Sandro Aguilar
 # A Ruby program
+# .index("SA") => returns index if it exists
+# .include?("CA") => returns T/F if its in the array
 
 arr = ["SA", "WA", "CA"]
 puts arr.include?("CA")
