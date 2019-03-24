881cb0b93eb8026b29b918e96a6de7c2b12630c1
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-23 20:33:28 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -2,6 +2,7 @@
 # A Ruby program
 # .index("SA") => returns index if it exists
 # .include?("CA") => returns T/F if its in the array
+# .reverse => reverses an array
 
 arr = ["SA", "WA", "CA"]
 puts arr.include?("CA")
