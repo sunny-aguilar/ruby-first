184f623893e6aff3bd26d98f4f9b851c9eab90ad
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-24 19:55:11 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -14,3 +14,8 @@ end
 
 puts first_in_array(["a", "b", "c", "d"], "d", "b"); # => "b"
 puts first_in_array(["cat", "bird" ,"dog", "mouse" ], "dog", "mouse"); # => "dog"
+
+# Write a method first_in_array that 
+# takes in an array and two elements, the 
+# method should return the element that 
+# appears earlier in the array.
