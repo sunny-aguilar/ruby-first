f308c6159ad80a0c3ab9dd0b327e06719576cb75
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-25 18:09:28 -0700

    ruby

--- ex01.rb
+++ ex01.rb
@@ -1,25 +1,46 @@
 # Author: Sandro Aguilar
 # A Ruby program
-# .index("SA") => returns index if it exists
+# .index("SA") => returns index if string exists
 # .include?("CA") => returns T/F if its in the array
 # .reverse => reverses an array
 # [n..N] => returns a range in an array including N/ ... exludes N
 # .each { |var| puts var }
 # .each do |var| *statements* end
 # .times {}
+# .each_char  => iterates over each character
 
-def first_in_array(arr, el1, el2)
-    arr.each do |ele|
-        if ele == el1 || ele == el2
-            return ele
-        end
+# Hint: use str.upcase and str.downcase
+# "abc".upcase # => "ABC"
+
+# def format_name(str)
+
+# end
+
+# puts format_name("chase WILSON") # => "Chase Wilson"
+# puts format_name("brian CrAwFoRd scoTT") # => "Brian Crawford Scott"
+
+# Write a method format_name that takes
+# in a name string and returns the name
+# properly capitalized.
+
+class Dog
+    def talk
+        puts "Woof!"
+    end
+
+    def name=(new_value)
+        @name = new_value
+    end
+
+    def name
+        @name
+    end
+
+    def report_name
+        puts "#{@name} is a super cool puppy"
     end
 end
 
-puts first_in_array(["a", "b", "c", "d"], "d", "b"); # => "b"
-puts first_in_array(["cat", "bird" ,"dog", "mouse" ], "dog", "mouse"); # => "dog"
-
-# Write a method first_in_array that
-# takes in an array and two elements, the
-# method should return the element that
-# appears earlier in the array.
+fido = Dog.new
+fido.name = "Fido"
+fido.report_name
