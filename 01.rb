2f083fc337f17baa97ed04aa697c1ab59e7c315f
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-27 19:12:50 -0700

    ruby

--- 01.rb
+++ 01.rb
@@ -2,5 +2,5 @@
 first, second, third = ARGV
 
 puts "Your first variable is: #{first}"
-puts "Your second variable is: #{}"
+puts "Your second variable is: #{second}"
 puts "Your third variable is: #{}"
